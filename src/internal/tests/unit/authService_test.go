de32141efd38df6e83d87a8922117d0923114c74
Andrew Razin <razin.andrew.ru@mail.ru>
2024-10-01 22:11:51 +0300

    finished testing

--- src/internal/tests/unit/authService_test.go
+++ src/internal/tests/unit/authService_test.go
@@ -2,13 +2,21 @@ package service_test
 
 import (
 	service "annotater/internal/bl/auth"
+
+	repository "annotater/internal/bl/userService/userRepo"
+	repo_adapter "annotater/internal/bl/userService/userRepo/userRepoAdapter"
 	mock_repository "annotater/internal/mocks/bl/userService/userRepo"
 	mock_auth_utils "annotater/internal/mocks/pkg/authUtils"
 	"annotater/internal/models"
+	auth_utils "annotater/internal/pkg/authUtils"
 	unit_test_utils "annotater/internal/tests/utils"
 	"testing"
 
+	"github.com/DATA-DOG/go-sqlmock"
 	"github.com/pkg/errors"
+	"github.com/stretchr/testify/require"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 
 	"github.com/golang/mock/gomock"
 	"github.com/ozontech/allure-go/pkg/framework/provider"
@@ -124,6 +132,214 @@ func (s *AuthServiceSuite) Test_AuthService_Auth(t provider.T) {
 	}
 }
 
+func (s *AuthServiceSuite) Test_AuthService_Auth_Classic(t provider.T) {
+	type fields struct {
+		userRepo       repository.IUserRepository
+		passwordHasher *mock_auth_utils.MockIPasswordHasher // IT IS BECAUSE OF NON DETERMINISTIC BEHAVIOUR
+		tokenizer      auth_utils.ITokenHandler
+		sqlMock        sqlmock.Sqlmock
+		key            string
+	}
+	type args struct {
+		Candidate models.User
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		prepare func(f *fields, a *args)
+		args    args
+		wantErr bool
+		err     error
+		want    *models.User
+	}{
+		{
+			name: "Valid Created User",
+			prepare: func(f *fields, a *args) {
+				mother := unit_test_utils.NewUserObjectMother()
+				user := mother.DefaultUser()
+				userHashed := mother.DefaultUserHashedPassWd()
+
+				f.passwordHasher.EXPECT().GenerateHash(user.Password).Return(userHashed.Password, nil)
+				f.sqlMock.ExpectBegin()
+				f.sqlMock.ExpectQuery(`INSERT INTO "users" ("login","password","name","surname","role","group") VALUES ($1,$2,$3,$4,$5,$6) RETURNING "id"`).
+					WithArgs(user.Login, userHashed.Password, user.Name, user.Surname, user.Role, user.Group).
+					WillReturnRows(sqlmock.NewRows([]string{"id"}).AddRow(1))
+				f.sqlMock.ExpectCommit()
+			},
+			args:    args{*unit_test_utils.NewUserObjectMother().DefaultUser()},
+			want:    nil,
+			wantErr: false,
+			err:     nil,
+		},
+		{
+			name: "No login",
+			prepare: func(f *fields, a *args) {
+
+			},
+			args:    args{*unit_test_utils.NewUserObjectMother().UserWithoutLogin()},
+			want:    nil,
+			wantErr: true,
+			err:     errors.Wrapf(service.ErrNoLogin, service.ERR_LOGIN_STRF, unit_test_utils.TEST_EMPTY_LOGIN),
+		},
+	}
+	ctrl := gomock.NewController(t)
+	for _, tt := range tests {
+		t.Title("Auth")
+		t.Tags("auth")
+		t.WithNewStep(tt.name, func(t provider.StepCtx) {
+
+			db, mock, err := sqlmock.New(sqlmock.QueryMatcherOption(sqlmock.QueryMatcherEqual))
+			require.NoError(t, err)
+			defer db.Close()
+
+			gormDB, err := gorm.Open(postgres.New(postgres.Config{Conn: db}), &gorm.Config{})
+			require.NoError(t, err)
+
+			userStorage := repo_adapter.NewUserRepositoryAdapter(gormDB)
+			passwordHasher := mock_auth_utils.NewMockIPasswordHasher(ctrl)
+			tokenizer := auth_utils.NewJWTTokenHandler()
+
+			if tt.prepare != nil {
+				tt.prepare(&fields{
+					userRepo:       userStorage,
+					passwordHasher: passwordHasher,
+					tokenizer:      tokenizer,
+					key:            unit_test_utils.TEST_HASH_KEY,
+					sqlMock:        mock,
+				},
+					&tt.args)
+			}
+
+			authService := service.NewAuthService(unit_test_utils.MockLogger, userStorage, passwordHasher, tokenizer, unit_test_utils.TEST_HASH_KEY)
+			err = authService.SignUp(&tt.args.Candidate)
+
+			t.WithNewParameters("Candidate", tt.args.Candidate, "err", err)
+			if tt.wantErr {
+				t.Assert().Error(err)
+				t.Assert().Equal(tt.err.Error(), err.Error())
+			} else {
+				t.Assert().NoError(err)
+			}
+		})
+	}
+}
+
+func (s *AuthServiceSuite) Test_AuthService_SignIn_Classic(t provider.T) {
+	type args struct {
+		Candidate models.User
+	}
+
+	type fields struct {
+		userRepo       repository.IUserRepository
+		passwordHasher *mock_auth_utils.MockIPasswordHasher
+		tokenizer      *mock_auth_utils.MockITokenHandler
+		sqlMock        sqlmock.Sqlmock
+		key            string
+	}
+
+	tests := []struct {
+		name    string
+		prepare func(f *fields) // Adjusted to only require fields
+		args    args
+		getWant func(f *fields) string
+		want    string
+		wantErr bool
+		err     error
+	}{
+		{
+			name: "Valid User",
+			prepare: func(f *fields) {
+				user := unit_test_utils.NewUserObjectMother().DefaultUser()
+				hashedPasswdUser := unit_test_utils.NewUserObjectMother().DefaultUserHashedPassWd()
+
+				rows := sqlmock.NewRows([]string{"id", "login", "password", "name", "surname", "role", "group"}).
+					AddRow(hashedPasswdUser.ID, hashedPasswdUser.Login, hashedPasswdUser.Password, hashedPasswdUser.Name, hashedPasswdUser.Surname, hashedPasswdUser.Role, hashedPasswdUser.Group)
+
+				f.sqlMock.ExpectQuery(`SELECT * FROM "users" WHERE login = $1 ORDER BY "users"."id" LIMIT $2`).
+					WithArgs(user.Login, 1).
+					WillReturnRows(rows)
+
+				f.passwordHasher.EXPECT().ComparePasswordhash(user.Password, hashedPasswdUser.Password).Return(nil)
+				f.tokenizer.EXPECT().GenerateToken(*hashedPasswdUser, unit_test_utils.TEST_HASH_KEY).Return(unit_test_utils.TEST_VALID_TOKEN, nil)
+
+			},
+			args:    args{Candidate: *unit_test_utils.NewUserObjectMother().DefaultUser()},
+			wantErr: false,
+			getWant: func(f *fields) string {
+
+				return unit_test_utils.TEST_VALID_TOKEN
+			},
+		},
+		{
+			name:    "No login",
+			prepare: nil,
+			args:    args{Candidate: *unit_test_utils.NewUserObjectMother().UserWithoutLogin()},
+			getWant: nil,
+			wantErr: true,
+			err:     errors.Wrapf(service.ErrNoLogin, service.ERR_LOGIN_STRF, unit_test_utils.TEST_EMPTY_LOGIN),
+		},
+	}
+
+	t.Title("SignIn")
+	t.Tags("auth")
+	ctrl := gomock.NewController(t)
+
+	for _, tt := range tests {
+		t.WithNewStep(tt.name, func(t provider.StepCtx) {
+			db, mock, err := sqlmock.New(sqlmock.QueryMatcherOption(sqlmock.QueryMatcherEqual))
+			require.NoError(t, err)
+			defer db.Close()
+
+			gormDB, err := gorm.Open(postgres.New(postgres.Config{Conn: db}), &gorm.Config{})
+			require.NoError(t, err)
+
+			repo := repo_adapter.NewUserRepositoryAdapter(gormDB)
+			tokenHandler := mock_auth_utils.NewMockITokenHandler(ctrl)     //nondeterm
+			passwordHasher := mock_auth_utils.NewMockIPasswordHasher(ctrl) // non deterministic
+
+			// Initialize fields for current test case
+			fields := fields{
+				userRepo:       repo,
+				tokenizer:      tokenHandler,
+				passwordHasher: passwordHasher,
+				sqlMock:        mock,
+				key:            unit_test_utils.TEST_HASH_KEY,
+			}
+
+			// Call prepare function if it exists
+			if tt.prepare != nil {
+				tt.prepare(&fields) // Pass the fields to the prepare function
+			}
+			if tt.getWant != nil {
+				tt.want = tt.getWant(&fields)
+			}
+
+			authService := service.NewAuthService(unit_test_utils.MockLogger, repo, passwordHasher, tokenHandler, unit_test_utils.TEST_HASH_KEY)
+
+			tokenStr, err := authService.SignIn(&tt.args.Candidate) // Call the SignIn method
+
+			t.WithNewParameters("Candidate", tt.args.Candidate, "err", err)
+
+			if tt.wantErr {
+				t.Assert().Error(err)
+				t.Assert().Equal(tt.err.Error(), err.Error())
+			} else {
+				t.Assert().NoError(err)
+				// wasnot working as intended 0_0
+				//lastDotIndexWant := strings.LastIndex(tt.want, ".")
+				//expectedTokenData := tt.want[1:lastDotIndexWant]
+
+				//lastDotIndexActual := strings.LastIndex(tokenStr, ".")
+				//actualTokenData := tokenStr[1:lastDotIndexActual]
+				t.Assert().Equal(tt.want, tokenStr)
+			}
+
+			// Ensure all expectations were met
+			require.NoError(t, mock.ExpectationsWereMet())
+		})
+	}
+}
+
 func (s *AuthServiceSuite) Test_AuthService_SignIn(t provider.T) {
 	type fields struct {
 		userRepo       *mock_repository.MockIUserRepository
@@ -228,7 +444,6 @@ func (s *AuthServiceSuite) Test_AuthService_SignIn(t provider.T) {
 		})
 	}
 }
-
 func TestAuthServiceSuiteRunner(t *testing.T) {
 	suite.RunSuite(t, new(AuthServiceSuite))
 }
